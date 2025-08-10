d824b1abf8f206a0b6a27896dc071b28dc602652
Marc Hadfield <marc@vital.ai>
2025-08-10 11:18:31 -0400

    sync

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kgraphmemory',
-    version='0.0.6',
+    version='0.0.7',
     author='Marc Hadfield',
     author_email='marc@vital.ai',
     description='KGraph Memory',
@@ -12,23 +12,26 @@ setup(
     packages=find_packages(exclude=["test"]),
     license='Apache License 2.0',
     install_requires=[
-            'vital-ai-vitalsigns>=0.1.26',
-            'vital-ai-domain>=0.1.6',
+            'vital-ai-vitalsigns>=0.1.32',
+            'vital-ai-domain>=0.1.7',
             'six',
             'pyyaml',
-            'vital-ai-haley-kg>=0.1.4',
-            'rdflib==7.0.0',
+            'vital-ai-haley-kg>=0.1.24',
+            'rdflib>=7.0.0',
             'SPARQLWrapper>=2.0.0',
             'networkx',
             'matplotlib',
             # 'transformers==4.41.2',
-            'kgraphservice>=0.0.7'
+            'kgraphservice>=0.0.8',
+            'scikit-learn',
+            'scipy',
+            'numpy'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.12',
 )
 
